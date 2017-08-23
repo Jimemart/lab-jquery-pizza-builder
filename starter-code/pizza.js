080a856a9c66fd84b187714c1f469cce080fd7b2
Jimena Martin <jimena.mg@hotmail.com>
2017-08-23 09:19:19 +0200

    Funciona la suma

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,10 +1,26 @@
 // Write your Pizza Builder JavaScript in this file.
+
+
 $(document).ready(function(){
-  /*---------------BOTONES------------*/
+
+  function totalPrice(){
+    var precio = 10;
+    var precioIndividual;
+    var visiblePrices = $(".price li:visible");
+    for(var i = 0; i<visiblePrices.length; i++){
+      precioIndividual = parseInt(visiblePrices[i].innerHTML.slice(1,2));
+      precio += precioIndividual;
+    }
+    return precio;
+  }
+
  $(".btn-pepperonni").on("click",function(){
     $(".pep").toggle();
     $(".btn-pepperonni").toggleClass("active");
     $(".price ul :nth-child(1)").toggle();
+    $("strong").text("$"+totalPrice());
+
+
 
   //   inicial.toggle(function(){
   //     inicial= inicial - precio;
@@ -22,39 +38,43 @@ $(document).ready(function(){
     $(".mushroom").toggle();
     $(".btn-mushrooms").toggleClass("active");
     $(".price ul :nth-child(2)").toggle();
+    $("strong").text("$"+totalPrice());
+
   });
 
   $(".btn-green-peppers").on("click", function(){
     $(".green-pepper").toggle();
     $(".btn-green-peppers").toggleClass("active");
     $(".price ul :nth-child(3)").toggle();
+    $("strong").text("$"+totalPrice());
   });
 
 
   $(".crust").removeClass("crust-gluten-free");
   $(".btn-crust").removeClass("active");
   $(".price ul :nth-child(5)").hide();
+  $("strong").text("$"+totalPrice());
 
   $(".btn-crust").on("click", function(){
     $(".crust").toggleClass("crust-gluten-free");
     $(".btn-crust").toggleClass("active");
     $(".price ul :nth-child(5)").toggle();
+    $("strong").text("$"+totalPrice());
   });
 
 
   $(".btn-sauce").removeClass("active");
   $(".sauce").removeClass("sauce-white");
   $(".price ul :nth-child(4)").hide();
+  $("strong").text("$"+totalPrice());
 
   $(".btn-sauce").on("click",function(){
     $(".sauce").toggleClass("sauce-white");
     $(".btn-sauce").toggleClass("active");
     $(".price ul :nth-child(4)").toggle();
+    $("strong").text("$"+totalPrice());
   });
 
-  function updatePrice(){
-    var precio = $("strong")[0].innerHTML.slice(1);
-    var ingredientes = $(".price ul li[style *='display']");
-    f
-  }
+      $("strong").text("$"+13);
+
 });
